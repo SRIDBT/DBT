d61ecbde2e471e41918e7258374206dd7cee2a8f
srikanth madapati <sridbt1@gmail.com>
2024-01-13 14:11:39 +0000

    ok

--- models/orders/orders_cust_total.sql
+++ models/orders/orders_cust_total.sql
@@ -4,4 +4,4 @@
 INNER JOIN ORDERS AS C ON A.C_CUSTKEY=C.O_CUSTKEY
 GROUP BY A.C_CUSTKEY,A.C_NAME,A.N_NAME,B.L_SHIPMODE,C.O_ORDERSTATUS)
 SELECT * FROM ORD
---dbt build --select orders_cust_total.sql
+--dbt build --select orders_cust_total.sql dbt build --models +orders_cust_total
